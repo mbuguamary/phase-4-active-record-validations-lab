29142a6757ff3dab6a61eb6fed476af83497a0f2
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-11-28 17:51:29 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+validates :title, presence: true
+validates :content, length: {minimum: 250}
+validates :summary, length: {maximum: 250}
+validates :category, inclusion: ["Fiction", "Non-Fiction"]
+validate :click?
+STRINGS = [/Secret/i, /Won't Believe/i, /Guess/i, /Top \d/i]
+def click?
+    if STRINGS.none? { |t| t.match title }
+    errors.add(:title, "must be clickbait")
+    end
+end
 end
